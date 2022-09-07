f13135ebbfeb49daba2f32f140aefe7883fccc3e
bmartin2013 <bmartin2013.git@gmail.com>
2022-09-07 16:34:11 -0300

    install bulma css

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // index.js
 import React from "react"
 import { createRoot } from 'react-dom/client';
+import App from "./App";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<h1>Hello World!</h1>);
+root.render(<App/>);
