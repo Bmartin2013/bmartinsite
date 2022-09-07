bdc81d6dad93aad0d023f1523a0f1aea1dfa703c
bmartin2013 <bmartin2013.git@gmail.com>
2022-09-07 16:54:53 -0300

    add fullpage simple sample

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import 'bulma/css/bulma.min.css';
+import ReactFullpage from '@fullpage/react-fullpage';
 
 const App = () => {
     return (
-      <div classname="main"> 
-        <div className="buttons">
-          <button class="button is-primary">Primary</button>
-          <button class="button is-link">Link</button>
-          <button class="button is-info">Info</button>
-          <button class="button is-success">Success</button>
-          <button class="button is-warning">Warning</button>
-          <button class="button is-danger">Danger</button>
-          <button class="button is-black">Black</button>
-          <button class="button is-white">White</button>
-          <button class="button is-dark">Dark</button>
-          <button class="button is-light">Light</button>
-        </div>
-      </div>
+        <ReactFullpage
+        //fullpage options
+        scrollingSpeed = {1000} /* Options here */
+    
+        render={({ state, fullpageApi }) => {
+          return (
+            <ReactFullpage.Wrapper>
+              <div className="section">
+                <p>Section 1 (welcome to fullpage.js)</p>
+              </div>
+              <div className="section">
+                <p>Section 2</p>
+              </div>
+            </ReactFullpage.Wrapper>
+          );
+        }}
+      />
     )
 }
 export default App;
