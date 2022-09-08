8eb9e634cdafeed7ac0b8f3a71ecec41202d5e60
bmartin2013 <bmartin2013.git@gmail.com>
2022-09-08 12:36:23 -0300

    footer wip

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import './src/styles/styles.css'
 import ReactFullpage from '@fullpage/react-fullpage';
 import FullPageSection from "./src/components/FullPageSection";
 import HeroBanner from "./src/components/HeroBanner";
+import Icon from '@mdi/react'
+import { mdiLinkedin, mdiGithub, mdiEmailOpenMultipleOutline,mdiGoogleDrive  } from '@mdi/js';
 
 const App = () => {
   return (
@@ -15,7 +17,39 @@ const App = () => {
             <FullPageSection children={<p>Section 2</p>} />
             <FullPageSection children={<p>Section 3</p>} />
             <footer id="footer">
-              reach me on the phone
+              <div id="sm-container">
+                <span>
+                  <Icon path={mdiLinkedin}
+                    title="User Profile"
+                    size={1} />
+                </span>
+                <span>
+                  <Icon path={mdiGithub}
+                    title="User Profile"
+                    size={1} />
+                </span>
+                <span>
+                  <Icon path={mdiEmailOpenMultipleOutline}
+                    title="User Profile"
+                    size={1} />
+                </span>
+                <span>
+                  <Icon path={mdiGoogleDrive}
+                    title="User Profile"
+                    size={1} />
+                </span>
+
+                {/* <span class="icon sm-icon">
+                  <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                </span>
+                <span class="icon sm-icon">
+                  <FontAwesomeIcon icon={faBlog} size="2x" />
+                </span>
+
+                <span class="icon sm-icon">
+                  <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                </span> */}
+              </div>
             </footer>
           </ReactFullpage.Wrapper>
         );
