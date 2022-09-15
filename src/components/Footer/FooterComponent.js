5e75b236012931a2839ca258fe768a5394edf7fb
Barbara Martinez <bmartin2013.git@gmail.com>
2022-09-15 07:37:18 -0500

    Update src/components/Footer/FooterComponent.js

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -3,7 +3,6 @@ import 'bulma/css/bulma.min.css';
 import './styles'
 import FooterBtn from "./FooterBtn";
 
-// TODO: Is there any way to automatically add buttons from metadata without adding a new component
 const FooterComponent = ({ socialMedia }) => {
   return (
     <div id="sm-container">
